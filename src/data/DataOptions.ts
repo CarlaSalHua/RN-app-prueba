cd1b6a7728725520695dfae116bd34103f5db11d
carlasalh <csalazar@visualsat.com>
2023-05-23 21:46:53 -0500

    Data-Options-update

--- src/data/DataOptions.ts
+++ src/data/DataOptions.ts
@@ -1,6 +1,31 @@
-export const DataOptions = [
+export const DataOptions: DataOptionI[] = [
   {
-    //img: require(''),
+    img: require('src/assets/car.png'),
     label: 'Carrito',
   },
+  {
+    img: require('src/assets/shopping.png'),
+    label: 'Compras',
+  },
+  {
+    img: require('src/assets/mask.png'),
+    label: 'Salud',
+  },
+  {
+    img: require('src/assets/ballon.png'),
+    label: 'Aniversario',
+  },
+  {
+    img: require('src/assets/cell.png'),
+    label: 'WhatsApp',
+  },
+  {
+    img: require('src/assets/box.png'),
+    label: 'Envío',
+  },
 ];
+
+interface DataOptionI {
+  img: NodeRequire;
+  label: string;
+}
